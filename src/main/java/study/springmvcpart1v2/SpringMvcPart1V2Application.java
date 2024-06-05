bc12982e6599a529209488b589fe188655be1c4f
Jisu Lee <jisulee.shsf@gmail.com>
2024-06-05 20:34:19 +0900

    Initial commit

---
+++ src/main/java/study/springmvcpart1v2/SpringMvcPart1V2Application.java
@@ -0,0 +1,13 @@
+package study.springmvcpart1v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPart1V2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcPart1V2Application.class, args);
+	}
+
+}
---
+++ src/test/java/study/springmvcpart1v2/SpringMvcPart1V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.springmvcpart1v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcPart1V2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
